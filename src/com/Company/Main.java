0348a94eb5e7e31f5caeeb396a7acafc8fe2cd5d
JuanitaCeci <juanitaceci0624@gmail.com>
2021-11-03 19:27:54 -0400

    the basic structure of the car

---
+++ src/com/Company/Main.java
@@ -0,0 +1,9 @@
+package com.Company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        car newcar= new car();
+        
+    }
+}
---
+++ src/com/Company/car.java
@@ -0,0 +1,11 @@
+package com.Company;
+
+public class car {
+
+    public String color;
+    public String model;
+    public int doors;
+    public String name;
+    public String engineCapacity;
+
+}
